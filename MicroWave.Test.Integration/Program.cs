362f73c24810c3741cb2f3f62373f9be67cad58e
Sophie Jørgensen Hansen <sophiejh94@gmail.com>
2021-04-22 16:11:33 +0200

    Tilføjede integrationstets projektet

---
+++ MicroWave.Test.Integration/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MicroWave.Test.Integration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
