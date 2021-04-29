0859f4e6394cd877fc1bd82c095ceca57f1a5698
Dina Bæk Andersen <au648834@uni.au.dk>
2021-04-29 16:56:47 +0200

    Påbegyndt integrationstest for CookController-Display.

---
+++ MicroWave.Test.Integration1/T7_CookControllerAndDisplay.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Microwave.Classes.Boundary;
+using Microwave.Classes.Controllers;
+using Microwave.Classes.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace MicroWave.Test.Integration
+{
+    public class T7_CookControllerAndDisplay
+    {
+        private CookController cookController;
+        private IDisplay display;
+        private ITimer timer;
+        private IPowerTube powerTube;
+        private IOutput output;
+        private StringWriter stringWriter; 
+
+        [SetUp]
+        public void SetUp()
+        {
+            powerTube = Substitute.For<IPowerTube>();
+            timer = new Timer();
+            output = new Output();
+            display = new Display(output);
+            cookController = new CookController(timer,display,powerTube);
+
+            stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+        }
+
+        //Flere testcases her. 
+        [TestCase("00","20")]
+        public void bla(string minutes, string seconds)
+        {
+            timer.Start(Convert.ToInt32(minutes) * 60 + Convert.ToInt32(seconds));
+            cookController.OnTimerTick(this,EventArgs.Empty); 
+            Assert.That(stringWriter.ToString().Contains(minutes + ":" + seconds));
+
+            //Der er noget galt med denne test. Vi har prøvet forskellige versioner, der alle fejler. 
+        }
+    }
+}
