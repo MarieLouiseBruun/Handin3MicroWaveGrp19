fbbccb6be4cebc0de5c6ffe6147c9d9c00124362
Sarah Amalie Vestmark Pind <au554980@uni.au.dk>
2021-04-29 10:58:06 +0200

    Integration test for Light and output

--- MicroWave.Test.Integration1/IT1_LightAndOutput.cs
+++ MicroWave.Test.Integration1/IT1_LightAndOutput.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using NUnit.Framework;
 using Microwave.Classes.Boundary;
@@ -9,23 +10,40 @@ namespace MicroWave.Test.Integration
     {
         private ILight light;
         private IOutput output;
-        private StringReader stringReader;
+        //private StringReader stringReader;
+        private StringWriter stringWriter;
 
         [SetUp]
         public void Setup()
         {
             output = new Output();
             light = new Light(output);
-            stringReader = new StringReader();
-            System.Console.SetOut(stringReader);
-            
+            //stringReader = new StringReader();
+            //System.Console.SetOut(stringReader);
+            stringWriter = new StringWriter();
+            System.Console.SetOut(stringWriter);
+
         }
 
         [Test]
-        public void Test1()
+        public void NoMethodCall_ConsoleIsEmpty()
         {
-            
-            Assert.Pass();
+            //Assert.That(stringWriter.ToString().IsNullOrEmpty());
+        }
+
+        [Test]
+        public void LightIsTurnedOn()
+        {
+            light.TurnOn();
+            Assert.That(stringWriter.ToString().Contains("on"));
+        }
+
+        [Test]
+        public void LightIsTurnedOff()
+        {
+            light.TurnOn();
+            light.TurnOff();
+            Assert.That(stringWriter.ToString().Contains("off"));
         }
     }
 }
